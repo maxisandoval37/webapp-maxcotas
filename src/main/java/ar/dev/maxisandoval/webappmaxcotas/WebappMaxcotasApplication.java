8b1a67855e0b2bd9cd384afaad2ada18d37958cd
Maxi Sandoval <maxisandoval98@gmail.com>
2024-02-20 21:15:26 -0300

    primer commit

---
+++ src/main/java/ar/dev/maxisandoval/webappmaxcotas/WebappMaxcotasApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.webappmaxcotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappMaxcotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappMaxcotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/dev/maxisandoval/webappmaxcotas/WebappMaxcotasApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.webappmaxcotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappMaxcotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
