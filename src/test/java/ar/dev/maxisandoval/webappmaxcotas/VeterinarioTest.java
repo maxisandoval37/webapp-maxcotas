2f6b6f4c836fd3fd9c989026a1edf3da46272fc6
Maxi Sandoval <maxisandoval98@gmail.com>
2024-02-27 19:49:28 -0300

    test de veterinario

---
+++ src/test/java/ar/dev/maxisandoval/webappmaxcotas/VeterinarioTest.java
@@ -0,0 +1,53 @@
+package ar.dev.maxisandoval.webappmaxcotas;
+
+import ar.dev.maxisandoval.webappmaxcotas.model.Veterinario;
+import ar.dev.maxisandoval.webappmaxcotas.service.VeterinarioService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class VeterinarioTest {
+
+    @Autowired
+    private VeterinarioService veterinarioService;
+
+    private Veterinario veterinarioGuardado;
+
+    @BeforeEach
+    void setup () {
+        Veterinario veterinario = new Veterinario();
+        veterinario.setNombre("Dr. Juan Diaz");
+        veterinario.setMatricula("ASD123");
+        veterinario.setEmail("drjuandiaz@example.com");
+
+        veterinarioGuardado = veterinarioService.guardarVeterinario(veterinario);
+    }
+
+    @Test
+    void testGuardarVeterinario() {
+        assertNotNull(veterinarioGuardado.getId());
+        assertEquals("Dr. Juan Diaz", veterinarioGuardado.getNombre());
+        assertEquals("ASD123", veterinarioGuardado.getMatricula());
+        assertEquals("drjuandiaz@example.com", veterinarioGuardado.getEmail());
+    }
+
+    @Test
+    void testListarVeterinario() {
+        List<Veterinario> veterinarios = veterinarioService.listarVeterinarios();
+        assertFalse(veterinarios.isEmpty());
+    }
+
+    @Test
+    void testObtenerVeterinarioPorId() {
+        Long veterinarioId = 1L;
+        Veterinario veterinario = veterinarioService.obtenerVeterinarioPorId(veterinarioId);
+
+        assertNotNull(veterinario);
+        assertEquals(veterinarioId, veterinario.getId());
+    }
+}
