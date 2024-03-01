546b5acdbc559f4bfe4262913d40e903883ceead
Maxi Sandoval <maxisandoval98@gmail.com>
2024-02-29 21:02:55 -0300

    agregado auth controller

---
+++ src/main/java/ar/dev/maxisandoval/webappmaxcotas/controller/AuthController.java
@@ -0,0 +1,44 @@
+package ar.dev.maxisandoval.webappmaxcotas.controller;
+
+import ar.dev.maxisandoval.webappmaxcotas.model.Usuario;
+import ar.dev.maxisandoval.webappmaxcotas.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class AuthController {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @GetMapping("/")
+    public String redireccionarPaginaPrincipal () {
+        return "redirect:/mascotas";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/registro")
+    public String mostrarFormularioRegistro(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String registrarUsuario(Usuario usuario){
+        customUserDetailsService.guardarUsuario(usuario);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/gestorRoles")
+    public String gestorRoles(Model model) {
+        model.addAttribute("usuarios", customUserDetailsService.listarUsuariosRegistrados());
+        return "gestorRoles";
+    }
+}
