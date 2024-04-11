ce545b8ea3f8c37d1d08acd01b67a9573d331363
Maxi Sandoval <maxisandoval98@gmail.com>
2024-04-11 10:27:21 -0300

    Refactor imports tests.
    Co-authored-by: maxisandoval37
    Co-authored-by: Maximiliano-Sandoval maxisandoval98@gmail.com

--- src/test/java/ar/dev/maxisandoval/webappmaxcotas/VeterinarioServiceTest.java
+++ src/test/java/ar/dev/maxisandoval/webappmaxcotas/VeterinarioServiceTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class VeterinarioServiceTest {
